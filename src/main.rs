2b4284f9d8eccaaf2f26f44851aab8d5636751fd
Mubelotix <mubelotix@gmail.com>
2021-10-03 00:28:23 +0200

    Start yew app

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
 use yew::prelude::*;
 
+enum Msg {}
 
+struct App {}
 
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {}
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                {"Hello World!"}
+            </div>
+        }
+    }
+}
 
 fn main() {
-    //yew::start_app::<Model>();
+    yew::start_app::<App>();
 }
