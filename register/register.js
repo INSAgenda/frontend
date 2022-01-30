97057fc4edf4fc3c054a59bf16490fe6b5a68ded
Mubelotix <mubelotix@gmail.com>
2022-01-30 01:42:15 +0100

    Improve css

--- register/register.js
+++ register/register.js
@@ -3,6 +3,7 @@ let error_element = document.getElementById("error-message");
 let email = document.getElementById("email-input");
 let password1 = document.getElementById("password-input1");
 let password2 = document.getElementById("password-input2");
+let stage = 1;
 
 async function submit() {
     if (password1.value != password2.value) {
@@ -30,7 +31,11 @@ async function submit() {
         localStorage.setItem('api_key', json.api_key);
         localStorage.setItem('counter', 1);
         window.location.replace("/agenda");
-    } else if (response.status == 400 || response.status == 500) {
+    } else if (response.status == 400 || response.status == 501) {
+        console.log("warning please set up to 500 add fucking handler");
+        stage = 2;
+        document.querySelector("main > form:nth-child(3)").style.display = "initial";
+
         let json = await response.json();
         error_element.innerHTML = json.message_fr; // TODO: display english messages
         error_element.style.display = "block";
@@ -38,9 +43,9 @@ async function submit() {
         alert("Unknown error");
     }
 };
-
 submit_el.onclick = submit;
 
+// Submit on enter
 document.onkeydown = async function(e) {
     if (e.code === "Enter") {
         if (email.value === "") {
@@ -60,4 +65,18 @@ document.onkeydown = async function(e) {
 
         await submit();
     }
+}
+
+// Animate radio inputs
+var radio = document.getElementsByClassName('language-checkbox-radio');
+for (var i = 0; i < radio.length; i++) {
+    radio[i].addEventListener('click', function () {
+        for (var i = 0; i < radio.length; i++) {
+            if (radio[i].checked == false) {
+                radio[i].parentElement.classList.remove('language-checkbox-active');
+            }
+        }
+        var parent = this.parentNode;
+        parent.classList.add('language-checkbox-active');
+    });
 }
