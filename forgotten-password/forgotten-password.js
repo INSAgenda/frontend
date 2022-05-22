2a3210a43a7aa80d25b45c340d332e7bda5b65fe
Mubelotix <mubelotix@gmail.com>
2022-05-22 15:53:23 +0200

    Finish forgotten password

--- forgotten-password/forgotten-password.js
+++ forgotten-password/forgotten-password.js
@@ -1,34 +1,35 @@
 let submit_el = document.getElementById("submit-button");
-let error_element = document.getElementById("error-message");
-let email = document.getElementById("email-input");
-let confirm_el = document.getElementById("confirm-message");
+let email_el = document.getElementById("email-input");
+let error_message_el = document.getElementById("error-message");
+let confirmation_message_el = document.getElementById("confirmation-message");
+
 async function submit() {
     let selector_list = [".submit-buttons", ".error-message-text"];
 
-    confirm_el.hidden = true;
-    if (email.value.length < 1) {
-        error_element.innerHTML = "Please enter an email address";
-        error_element.style.display = "block";
+    confirmation_message_el.style.display = "none";
+    if (email_el.value.lenght === 0) {
+        error_message_el.innerHTML = "Please enter an email address";
+        error_message_el.style.display = "block";
         return false;
-    }else{
-        error_element.style.display = "none";
+    } else {
+        error_message_el.style.display = "none";
     }
 
     enable_activity_indicator(selector_list, true);
     let response = await fetch('/api/auth/send-password-reset-email', {
         method: 'POST',
-        body: "email=" + encodeURIComponent(email.value),
+        body: "email=" + encodeURIComponent(email_el.value),
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
         },
     });
 
     if (response.status == 200) {
-        confirm_el.hidden = false;
+        confirmation_message_el.style.display = "block";
     } else if (response.status == 400 || response.status == 500) {
         let json = await response.json();
-        error_element.innerHTML = json.message_fr; // TODO: display english messages
-        error_element.style.display = "block";
+        error_message_el.innerHTML = json.message_fr; // TODO: display english messages
+        error_message_el.style.display = "block";
     } else {
         alert("Unknown error");
     }
@@ -39,8 +40,8 @@ submit_el.onclick = submit;
 
 document.onkeydown = async function(e) {
     if (e.code === "Enter") {
-        if (email.value === "") {
-            email.focus();
+        if (email_el.value === "") {
+            email_el.focus();
             return;
         }
         await submit();
