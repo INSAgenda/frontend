65325f823c004792bc54dee5929029be51afca8c
Mubelotix <mubelotix@gmail.com>
2022-12-03 17:01:55 +0100

    Formatting

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,37 +1,33 @@
-const storageKey = 'setting-theme'
+const storageKey = 'setting-theme';
+const authoThemeKey = 'auto-theme';
 
 const getSystemPreference = () => {
-  return window.matchMedia('(prefers-color-scheme: dark)').matches
-      ? 'dark'
-      : 'light'
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 }
 
 const getColorPreference = () => {
-  if (localStorage.getItem(storageKey))
-    return localStorage.getItem(storageKey)
-  else
-    return getSystemPreference()
+    if (localStorage.getItem(storageKey))
+        return localStorage.getItem(storageKey);
+    else
+        return getSystemPreference();
 }
 
-document.reflectTheme = reflectPreference = function (){
-  const authoThemeKey = 'auto-theme'
-  if ((localStorage.getItem(authoThemeKey)) && (localStorage.getItem(authoThemeKey) === 'true')) {
-    document.firstElementChild.setAttribute('data-theme', getSystemPreference());
-  } else {
-    document.firstElementChild.setAttribute('data-theme', getColorPreference())
-  }
+document.reflectTheme = reflectPreference = function () {
+    if ((localStorage.getItem(authoThemeKey)) && (localStorage.getItem(authoThemeKey) === 'true')) {
+        document.firstElementChild.setAttribute('data-theme', getSystemPreference());
+    } else {
+        document.firstElementChild.setAttribute('data-theme', getColorPreference())
+    }
 }
 
 document.reflectTheme()
-
 window.onload = () => {
-  document.reflectTheme()
+    document.reflectTheme()
 }
 
 window
-  .matchMedia('(prefers-color-scheme: dark)')
-  .addEventListener('change', ({matches:isDark}) => {   
-    // TODO: same call in webapp when selecting auto-theme
-    reflectPreference()
-  })
-
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', ({matches:isDark}) => {     
+        // TODO: same call in webapp when selecting auto-theme
+        reflectPreference()
+    })
